ac200af9147fa480de8118ac531ac420bcb4f0ff
srilekha2452 <srilekha3890@gmail.com>
2025-04-28 09:56:10 +0530

    modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // eslint-disable-next-line no-undef
+  base : process.env.VITE_BASE_PATH
 })
